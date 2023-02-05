ae34f0342a16d1979003b5a62ba543dd2ec8034f
parmsam <parmartsam@gmail.com>
2023-02-05 11:55:13 -0500

    get url search params

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 var ageEl = document.getElementById("age")
 var nameEl = document.getElementById("bday-name")
 
-function GetURLParameter(sParam) {
-    var sPageURL = window.location.search.substring(1);
-    var sURLVariables = sPageURL.split('&');
-    for (var i = 0; i < sURLVariables.length; i++) {
-        var sParameterName = sURLVariables[i].split('=');
-        if (sParameterName[0] == sParam) {
-            return sParameterName[1];
-        }
-    }
-}
-const name = GetURLParameter("name");
-if(urlParams.has('name')){
-    nameEl.textContent = nameEl;
-}
 nameEl.textContent = "Jim";
 ageEl.textContent = 15;
+
+const params = new URLSearchParams(window.location.q); //parse params
+const name_param = params.get("name"); //get q param
+const age_param = params.get("age"); //get q param
+if ( isEmpty(name_param) ) {
+    nameEl.textContent = name_param; //assign q value to input when loading
+    ageEl.textContent = age_param;
+} 
+
 
