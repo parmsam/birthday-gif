92aeaaace02465bf2740c5cbbbb8265f94a5e90d
parmsam <parmartsam@gmail.com>
2023-02-05 12:05:23 -0500

    fix getting url params

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var nameEl = document.getElementById("bday-name")
 nameEl.textContent = "Jim";
 ageEl.textContent = 15;
 
-const params = new URLSearchParams(window.location.q); //parse params
-const name_param = params.get("name"); //get q param
-const age_param = params.get("age"); //get q param
-if ( isEmpty(name_param) ) {
+const urlParams = new URL(window.location.toLocaleString());
+const name_param = urlParams.get("name"); //get q param
+const age_param = urlParams.get("age"); //get q param
+if ( name_param != '' ) {
     nameEl.textContent = name_param; //assign q value to input when loading
     ageEl.textContent = age_param;
 } 
