85004d48d1b4f644d1ba382b13d25faad1b85af5
parmsam <parmartsam@gmail.com>
2023-02-05 12:09:07 -0500

    get search params

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ var nameEl = document.getElementById("bday-name")
 nameEl.textContent = "Jim";
 ageEl.textContent = 15;
 
-const urlParams = new URL(window.location.toLocaleString());
-const name_param = urlParams.get("name"); //get q param
-const age_param = urlParams.get("age"); //get q param
-if ( name_param != '' ) {
-    nameEl.textContent = name_param; //assign q value to input when loading
+const urlParams = new URL(window.location.toLocaleString()).searchParams;
+const name_param = urlParams.get("name"); //get name param
+const age_param = urlParams.get("age"); //get age param
+if ( urlParams.has('name')) {
+    nameEl.textContent = name_param;
     ageEl.textContent = age_param;
 } 
-
-
